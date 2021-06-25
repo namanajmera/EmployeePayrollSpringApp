f93c1e3037d7b0a55e33ab226b756ecee97a2c63
Naman <cool.naman.ajmera@gmail.com>
2021-06-25 17:38:15 +0530

    Added: Added Some Properties to DTO and Model

--- src/main/java/com/naman/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/naman/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,15 +1,25 @@
 package com.naman.employeepayrollapp.dto;
 
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
-public class EmployeePayrollDTO {
+public @ToString
+class EmployeePayrollDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Employee name Invalid")
     public String name;
 
     @Min(value = 500, message = "Min Wage Should Be more Than 500")
     public long salary;
+
+    public String gender;
+    public String startDate;
+    public String note;
+    public String profilePic;
+    public List<String> departments;
 
     public EmployeePayrollDTO(String name, long salary) {
         this.name = name;
--- src/main/java/com/naman/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/naman/employeepayrollapp/model/EmployeePayrollData.java
@@ -2,10 +2,17 @@ package com.naman.employeepayrollapp.model;
 
 import com.naman.employeepayrollapp.dto.EmployeePayrollDTO;
 
+import java.util.List;
+
 public class EmployeePayrollData {
     private int empId;
     private String name;
     private long salary;
+    public String gender;
+    public String startDate;
+    public String note;
+    public String profilePic;
+    public List<String> departments;
 
     public EmployeePayrollData() {
     }
@@ -14,6 +21,11 @@ public class EmployeePayrollData {
         this.empId = empId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
+        this.gender= employeePayrollDTO.gender;
+        this.startDate= employeePayrollDTO.startDate;
+        this.note= employeePayrollDTO.note;
+        this.profilePic= employeePayrollDTO.profilePic;
+        this.departments= employeePayrollDTO.departments;
     }
 
     public int getEmpId() {

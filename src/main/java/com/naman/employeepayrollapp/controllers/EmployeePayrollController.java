8b66853e080e41c4c1d393d0603251e12ebaf1b8
Naman <cool.naman.ajmera@gmail.com>
2021-06-25 17:06:07 +0530

    Added: Add Some Validation to Name and Salary

--- src/main/java/com/naman/employeepayrollapp/controllers/EmployeePayrollController.java
+++ src/main/java/com/naman/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -36,7 +37,7 @@ public class EmployeePayrollController {
 
     //    Post Call
     @PostMapping("/post/create")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO employeePayrollDTO) {
         EmployeePayrollData employeePayrollData=iEmployeePayrollService.createEmployeePayrollData(employeePayrollDTO);
         ResponseDTO responseDTO = new ResponseDTO("Successfully Created Employee", employeePayrollData);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
@@ -44,7 +45,7 @@ public class EmployeePayrollController {
 
     //    Put Call with ID
     @PutMapping("/put/update/{empId}")
-    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,@Valid @RequestBody EmployeePayrollDTO employeePayrollDTO) {
         EmployeePayrollData employeePayrollData=iEmployeePayrollService.updateEmployeePayrollData(empId,employeePayrollDTO);
         ResponseDTO responseDTO = new ResponseDTO("Updated successfully", employeePayrollData);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
--- src/main/java/com/naman/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/naman/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,7 +1,14 @@
 package com.naman.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Employee name Invalid")
     public String name;
+
+    @Min(value = 500, message = "Min Wage Should Be more Than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
